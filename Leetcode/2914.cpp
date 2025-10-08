2041a5937ac26c63b2356a4b8d84b7045c18ae41
Dev-Gaurav-3 <itzgaurav003@gmail.com>
2025-10-08 08:23:51 +0530

    add

--- Leetcode/2914.cpp
+++ Leetcode/2914.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main()
 {
-
+    string s = "10101011101";
+    int ans = 0;
+    for (int i = 0; i < s.size(); i += 2)
+    {
+        ans += (s[i] != s[i + 1]);
+    }
     return 0;
 }
