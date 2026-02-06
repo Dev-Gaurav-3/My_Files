e4c5f09523f496f061980d5162f991999b9d2d1f
Dev-Gaurav-3 <itzgaurav003@gmail.com>
2026-02-06 20:15:01 +0530

    add

---
+++ Leetcode/2232.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s = "247+38";
+    s.insert(s.begin(),'(');
+    s += ')';
+
+    string ans = s;
+
+    
+
+
+
+  return 0;
+}
