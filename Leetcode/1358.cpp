e8b42520b95b56ccb0f79181c7db2f39074205f6
Dev-Gaurav-3 <itzgaurav003@gmail.com>
2026-06-30 20:02:09 +0530

    add

--- Leetcode/1358.cpp
+++ Leetcode/1358.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 
 int main(){
+    string s = "abcabc";
     
+    unordered_map<char, vector<int>> mp;
+
+        for (int i = 0; i < s.size(); i++) mp[s[i]].push_back(i);
+
+        int ans = 0;
+        for (int i = 0; i < s.size(); i++) {
+            char ch2, ch3;
+            if (s[i] == 'a') ch2 = 'b', ch3 = 'c';
+            else if (s[i] == 'b') ch2 = 'a', ch3 = 'c';
+            else ch2 = 'a', ch3 = 'b';
+
+            int p1 = upper_bound(mp[ch2].begin(), mp[ch2].end(), i) - mp[ch2].begin();
+            int p2 = upper_bound(mp[ch3].begin(), mp[ch3].end(), i) - mp[ch3].begin();
+            if (p1 == mp[ch2].size() || p2 == mp[ch3].size()) continue;
+            int mx = max(mp[ch2][p1], mp[ch3][p2]);
+            ans += s.size() - mx;
+        }
+        return ans;
+    
+    
+
     return 0;
 }
